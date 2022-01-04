baf377a2a2bd75d826aa37e68f2165a429eecf2f
AleFraMa <aleframa.cws@gmail.com>
2022-01-04 23:13:08 +0100

    add logo

--- src/components/layout-components/Logo.js
+++ src/components/layout-components/Logo.js
@@ -5,18 +5,19 @@ import { connect } from "react-redux";
 import utils from 'utils';
 import { Grid } from 'antd';
 
+import logo from './logo.svg'
 const { useBreakpoint } = Grid;
 
 const getLogoWidthGutter = (props, isMobile) => {
   const { navCollapsed, navType } = props;
   const isNavTop = navType === NAV_TYPE_TOP ? true : false
-  if(isMobile && !props.mobileLogo) {
+  if (isMobile && !props.mobileLogo) {
     return 0
   }
-  if(isNavTop) {
+  if (isNavTop) {
     return 'auto'
   }
-  if(navCollapsed) {
+  if (navCollapsed) {
     return `${SIDE_NAV_COLLAPSED_WIDTH}px`
   } else {
     return `${SIDE_NAV_WIDTH}px`
@@ -25,21 +26,21 @@ const getLogoWidthGutter = (props, isMobile) => {
 
 const getLogo = (props) => {
   const { navCollapsed, logoType } = props;
-  if(logoType === 'light') {
-    if(navCollapsed) {
-      return '/img/logo-sm-white.png'
+  if (logoType === 'light') {
+    if (navCollapsed) {
+      return logo
     }
-    return '/img/logo-white.png'
+    return logo
   }
 
   if (navCollapsed) {
-    return '/img/logoArrBimb.png'
+    return logo
   }
-  return '/img/prova.png'
+  return logo
 }
 
 const getLogoDisplay = (isMobile, mobileLogo) => {
-  if(isMobile && !mobileLogo) {
+  if (isMobile && !mobileLogo) {
     return 'd-none'
   } else {
     return 'logo'
@@ -50,15 +51,16 @@ export const Logo = (props) => {
   const isMobile = !utils.getBreakPoint(useBreakpoint()).includes('lg')
   return (
     <div
-      className={getLogoDisplay(isMobile, props.mobileLogo)} 
-      style={{width: `${getLogoWidthGutter(props, isMobile)}`}}>
-      <img src={getLogo(props)} alt={`${APP_NAME} logo`}/>
+      className={getLogoDisplay(isMobile, props.mobileLogo)}
+    //style={{width: `${getLogoWidthGutter(props, isMobile)}`}}
+    >
+      <img src={getLogo(props)} alt={`${APP_NAME}`} style={{ maxWidth: `100%`, maxHeight: `100%` }}/>
     </div>
   )
 }
 
 const mapStateToProps = ({ theme }) => {
-  const { navCollapsed, navType } =  theme;
+  const { navCollapsed, navType } = theme;
   return { navCollapsed, navType }
 };
 
