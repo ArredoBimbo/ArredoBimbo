2757f1af605a05a0c8391bcd208de4f3e08c29d5
AleFraMa <aleframa.cws@gmail.com>
2023-01-13 14:48:53 +0100

    add www

--- src/configs/IP.js
+++ src/configs/IP.js
@@ -1,4 +1,4 @@
-export const IP = 'https://arredobimbo.com:8443';
+export const IP = 'https://www.arredobimbo.com:8443';
 //export const IP = 'http://studio-manudaniel.ddns.net:8000';
 //export const IP ='http://localhost:8000'
 //export const IP = 'https://studio-manudaniel.ddns.net:8443'
